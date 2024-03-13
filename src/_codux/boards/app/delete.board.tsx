c4b2dc9e93329e2886ba62b80613ea132551b75a
albertCamusNoCode <128751182+albertCamusNoCode@users.noreply.github.com>
2024-03-13 14:15:15 -0600

    as

--- src/_codux/boards/app/delete.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-
-export default createBoard({
-    name: 'DELETE',
-    Board: () => <div></div>,
-    isSnippet: true,
-});
---
+++ src/components/test/test.tsx
@@ -0,0 +1,10 @@
+import '../../tailwind.css';
+import React from 'react';
+
+export interface TestProps {
+    className?: string;
+}
+
+export const Test: React.FC<TestProps> = ({ className = '' }) => (
+    <div className={`${className}`}>Test</div>
+);
