417df307e72a47d44835e0e45ee3c2f9315849f3
albertCamusNoCode <128751182+albertCamusNoCode@users.noreply.github.com>
2024-03-13 15:41:42 -0600

    Test

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -1,11 +1,8 @@
 import { createBoard } from "@wixc3/react-board";
-import App from "../../../App";
+import App from "@/App";
 
 export default createBoard({
   name: "App",
   Board: () => <App />,
   isSnippet: true,
-  environmentProps: {
-    windowWidth: 300,
-  },
 });
