37c43f570a047d9d55dd08ae53ac054c232b607c
albertCamusNoCode <128751182+albertCamusNoCode@users.noreply.github.com>
2024-03-13 15:24:32 -0600

    asd

--- src/_codux/boards/radix-components/dropdown-menu.board.tsx
+++ src/_codux/boards/radix-components/dropdown-menu.board.tsx
@@ -1,7 +1,12 @@
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
 import { createBoard } from "@wixc3/react-board";
 
 export default createBoard({
   name: "Dropdown Menu",
-  Board: () => <div></div>,
+  Board: () => (
+    <div>
+      <DropdownMenu>This is a Dropdown Menu</DropdownMenu>
+    </div>
+  ),
   isSnippet: true,
 });
