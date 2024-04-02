1e280d362d73099435c94407e7b8b1731545397e
albertCamusNoCode <128751182+albertCamusNoCode@users.noreply.github.com>
2024-04-01 19:42:47 -0600

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import ReactDOM from "react-dom";
 import App from "./App"; // Import App from App.tsx
 import "./tailwind.css";
 import "@fontsource/inter";
-// Import your publishable keys
 
 ReactDOM.render(<App />, document.getElementById("root"));
